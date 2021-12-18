05a3e2ebc650f4cca84b1f53d5f61570a23fff78
phmo <phillipm31415@gmail.com>
2021-12-17 16:17:00 -0800

    16...

--- day16.py
+++ day16.py
@@ -1,87 +1,95 @@
-import sys
-with open(sys.argv[1]) as raw_data:
-    hex_packet = raw_data.read().strip()
-bits = hex_packet
-# bits = "D2FE28"
-PACKET = "".join(map(lambda bit: bin(int(bit, 16))[2:].zfill(4), bits))
+class BinaryParser:
+    def __init__(self, hexstr):
+        self.binary = hex2binstr(hexstr)
+        self.parsed = []
+        self.ind = 0
+        self.maxind = len(self.binary)
 
-def read_literal(subpack):
-    """
-    VVV TTT AAAAA BBBBB CCCCC
-    version = subpack[:3]
-    type_id = subpack[3:6]
-    d1 = subpack[6:11]
-    d2 = subpack[11:16]
-    d3 = subpack[16:21]
-    extra = [21:]
+    def parse_literal(self):
+        """ parse AAAAA BBBBB CCCCC to int """
+        has_zgroup = False
+        final_bin = ""
+        while not has_zgroup:
+            if self.binary[self.ind] == '0':
+                has_zgroup = True
+            self.ind += 1
+            bin_group = self.binary[self.ind: self.ind + 4]
+            self.ind += 4
+            final_bin += bin_group
+        literal_value = bin2int(final_bin)
+        return literal_value
 
-    print(version, type_id, d1, d2, d3, extra)
-    """
-    return
+    def parse_operator(self) -> list:
+        """ parse I L...L """
+        len_type = self.binary[self.ind]
+        self.ind += 1
+        if len_type == '0':
+            num = 15
+            startbit = self.ind
+        elif len_type == '1':
+            num = 11
+        len_argb = self.binary[self.ind: self.ind + num]
+        self.ind += num
+        len_argc = bin2int(len_argb)
 
-def parse_operator(pack, ind, version, type_id, len_type_id):
-    """
-    len_type_id 0 : -> 15 - total length of bits in subpacket
-    len_type_id 1 : -> 11 - number of subpackets immediately contained
-    """
-    c_ind = ind + 7 + (11 if len_type_id else 15)
-    if len_type_id == 0:
-        total_bits = int(pack[ind + 7: ind + 22], 2)
-        while total_bits:
-            s_vers = pack[c_ind: c_ind+3]
-            s_type = pack[c_ind + 3: c_ind + 6]
-            if s_type == 4:
-                c_ind += 21
-                total_bits -= 21
-                yield [s_vers, s_type] 
-            else:
-                s_lti = pack[c_ind + 6]
-                parse_operator(pack, c_ind, s_vers, s_type, s_lti)
+        count = 0
+        subpackets = []
+        while count < len_argc:
+            subpacket = self.parse_data()
+            if len_type == '0':
+                count = self.ind - startbit
+            elif len_type == '1':
+                count += 1
+            subpackets.append(subpacket)
+        return subpackets
 
-    else:
-        subpackets_remaining = int(pack[ind + 7: ind + 18], 2)
-
-
-def parse_packet(pack):
-    """
-    type 4: literal
-    type _: operator
-        length_type 0: 15
-        length_type 1: 11
-    """
-     # oper = {0: len("VVVTTTILLLLLLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBBBBBB"),
-            # 1: len("VVVTTTILLLLLLLLLLLAAAAAAAAAAABBBBBBBBBBBCCCCCCCCCCC")}
-    print("!", pack, "!")
-    ind = 0
-    while ind < len(pack):
-        print(pack[ind:])
-        version = int(pack[ind: ind + 3], 2)
-        type_id = int(pack[ind + 3: ind + 6], 2)
-        jump = 0
-
+    def parse_data(self) -> list:
+        """ parse VVV TTT ... """
+        version = bin2int(self.binary[self.ind: self.ind + 3])
+        self.ind += 3
+        type_id = bin2int(self.binary[self.ind: self.ind + 3])
+        self.ind += 3
+        subpackets = []
+        # print("v, t:", version, type_id)
         if type_id == 4:
-            jump = 21
-            print("literal")
-            read_literal(pack[ind: ind + jump])
+            subpacket = self.parse_literal()
         else:
-            len_type_id = int(pack[ind + 6])
-            parse_operator(pack, ind, version, type_id, len_type_id)
+            subpacket = self.parse_operator()
+        subpackets.append((version, type_id, subpacket))
+        return subpackets
 
-        if jump:
-            ind += jump
-            while ind < len(pack) and pack[ind] == "0":
-                ind += 1
-        break
+    def parse_all(self) -> list:
+        subpackets = []
+        while self.ind < self.maxind - 11:
+            subpacket = self.parse_data()
+            subpackets.append(subpacket)
+            # self.skip_zeros()
+        return subpackets
 
-    # print(solution1)
-    # 597 (low)
+    def skip_zeros(self):
+        while self.ind < self.maxind and self.binary[self.ind] == '0':
+            self.ind += 1
+def hex2binstr(hexstr):
+    return "".join(map(lambda bit: bin(int(bit, 16))[2:].zfill(4), hexstr))
 
-test_1 = "D2FE28" # literal : (2021)
-test_2 = "38006F45291200" # operator : LT 0, 2 SP
-test_3 = "EE00D40C823060" # operator : LT 1, 3 SP
-# parse_packet(PACKET)
-def h2b(h):
-    return "".join(map(lambda bit: bin(int(bit, 16))[2:].zfill(4), h))
+def bin2int(binstr):
+    return int(binstr, 2)
 
-parse_packet(h2b(test_2))
+def main():
+    tests = ["D2FE28", # LITERAL 2021
+             "38006F45291200", # LT 0, 2 SP LITERALS
+             "EE00D40C823060", # LT 1, 3 SP LITERALS
+             "8A004A801A8002F478", # OP [OP [OP [LITERAL]]] : V = 16
+             "620080001611562C8802118E34", # OP [OP [2 LIT] * 2] V = 23
+             "C0015000016115A2E0802F182340", # same structure as above v = 23
+             "A0016C880162017C3686B18A3D4780", # OP [LIT * 5] v = 31
+            ]
+    for hexcase in tests:
+        print(hexcase)
+        print(hex2binstr(hexcase))
+        parser = BinaryParser(hexcase)
+        data = parser.parse_all()
+        print(data)
 
+if __name__ == "__main__":
+    main()

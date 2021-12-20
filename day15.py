908fd50f78e67bc7d1f8cb96020ea56f918cce43
phmo <phillipm31415@gmail.com>
2021-12-19 18:29:44 -0800

    solve day 15 part 2, slow ?

--- day15.py
+++ day15.py
@@ -33,21 +33,25 @@ def dijkstra(graph, source, edges):
 with open(sys.argv[1]) as raw_data:
     grid = [[int(num) for num in line.strip()] for line in raw_data]
 
-gd = {(ny, nx): node for ny, row in enumerate(grid) for nx, node in enumerate(row)}
-nbrs = {node: get_neighbors(node, len(grid), len(grid[0])) for node in gd}
-src = (0, 0)
-#solved = dijkstra(gd, src, nbrs)
-#print(solved[(len(grid) - 1, len(grid[0]) - 1)])
-# 673
+def solve(grid):
+    gd = {(ny, nx): node for ny, row in enumerate(grid) for nx, node in enumerate(row)}
+    nbrs = {node: get_neighbors(node, len(grid), len(grid[0])) for node in gd}
+    src = (0, 0)
+    solved = dijkstra(gd, src, nbrs)
+    print(solved[(len(grid) - 1, len(grid[0]) - 1)])
+    
 
 def expand(matrix: list[list]) -> list[list]:
     result = []
-    for row in matrix:
-        new_row = [(x+i) % 10 for i in range(5) for x in row]
-        result.append(new_row)
+    for i in range(5):
+        for row in matrix:
+            new_row = [x+i+j if x+i+j<10 else (x+i+j)%10+1 for j in range(5) for x in row]
+            result.append(new_row)
     return result
 
+# solve(grid)
+# 673
 large = expand(grid)
-for row in large:
-    print(row)
+solve(large) # SLOW
+# 2893
 
